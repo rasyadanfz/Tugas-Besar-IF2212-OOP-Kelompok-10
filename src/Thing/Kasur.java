2bf53eb350fd73bc1d32e40bc8394cc2fbfa7689
samuel-eric <30772879+samuel-eric@users.noreply.github.com>
2023-04-18 21:02:27 +0700

    membuat file Kasur.java dan KasurSingle.java

---
+++ src/Thing/Kasur.java
@@ -0,0 +1,7 @@
+package src.Thing;
+
+public abstract class Kasur extends Thing{
+    public Kasur(String nama, int panjang, int lebar, int harga) {
+        super(nama, panjang, lebar, harga);
+    }
+}
--- src/Thing/Thing.java
+++ src/Thing/Thing.java
@@ -1,4 +1,4 @@
-package src;
+package src.Thing;
 import java.util.*;
 
 public abstract class Thing {
