4b6b2393042c1eb2b1cb0e10dab406dae3d85908
carissa <carissazahrani@gmail.com>
2023-04-18 20:59:14 +0700

    coba interface purchaseable

---
+++ Ingredient.java
@@ -0,0 +1,11 @@
+public abstract class Ingredient{
+    private String nama;
+    private int price;
+    private int kekenyangan;
+
+    public Ingredient(String nama, int price, int kekenyangan){
+        this.nama = nama;
+        this.price = price;
+        this.kekenyangan = kekenyangan;
+    }
+}
