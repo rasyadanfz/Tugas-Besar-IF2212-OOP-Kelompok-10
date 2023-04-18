2f1dfd00b81591fd318b9edefa8b980ca7bef897
samuel-eric <30772879+samuel-eric@users.noreply.github.com>
2023-04-18 20:42:01 +0700

    tambah konstruktor dan getter untuk Thing.java

--- src/Thing.java
+++ src/Thing.java
@@ -6,4 +6,26 @@ public abstract class Thing {
     private int panjang;
     private int lebar;
     private int harga;
+
+    // konstruktor
+    public Thing(String nama, int panjang, int lebar, int harga) {
+        this.nama = nama;
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.harga = harga;
+    }
+
+    // getter
+    public String getNama() {
+        return nama;
+    }
+    public int getPanjang() {
+        return panjang;
+    }
+    public int getLebar() {
+        return lebar;
+    }
+    public int getHarga() {
+        return harga;
+    }
 }
