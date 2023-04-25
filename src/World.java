1fb7cafd35e25210a669e580256936a3262a1015
rasyadanfz <18221103@itb.ac.id>
2023-04-25 22:43:08 +0700

    add getHouse method

--- src/World.java
+++ src/World.java
@@ -1,6 +1,8 @@
 package src;
 import java.util.*;
 
+import src.Exceptions.HouseNotFoundException;
+
 public class World {
     private Matrix map;
     private ArrayList<House> daftarRumah;
@@ -28,5 +30,18 @@ public class World {
     public void addHouse(int x, int y, House house) {
         map.changeItem(x, y, house.getKodeRumah());
         daftarRumah.add(house);
+    }
+
+    public House getHouse(String kodeRumah) throws HouseNotFoundException{
+        Iterator<House> iterator = daftarRumah.iterator();
+        // Kalau ada rumah dengan kode yang ditentukan, return reference to Rumah itu
+        // else, throw HouseNotFoundException
+        while (iterator.hasNext()){
+            if (iterator.next().getKodeRumah().equals(kodeRumah)){
+                return iterator.next();
+            }
+        }
+        throw new HouseNotFoundException("Rumah dengan kode " + kodeRumah + " tidak ada!");
+        
     }
 }
