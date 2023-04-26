25403b68e861998ada53f4c252da8147cda5260b
cathlauretta <cathlauretta@gmail.com>
2023-04-26 17:33:11 +0700

    modified House & Room

--- src/House.java
+++ src/House.java
@@ -3,12 +3,18 @@ import java.util.*;
 
 public class House {
     private String kodeRumah;
+    private Matrix petaRumah = new Matrix(9, 9);
     private ArrayList<Room> daftarRuangan;
     private Point lokasi;
 
     public House(String kodeRumah) {
         this.kodeRumah = kodeRumah;
         daftarRuangan = new ArrayList<>();
+
+        Room firstRoom = new Room("R1", this);
+        daftarRuangan.add(firstRoom);
+
+        petaRumah.changeItem(5, 5, firstRoom.getNamaRuangan());
     }
 
     public String getKodeRumah() {
@@ -19,11 +25,23 @@ public class House {
         return lokasi;
     }
 
+    public void printPetaRumah() {
+        petaRumah.printMatrix();
+    }
+
     public ArrayList<Room> getSeluruhRuangan() {
         return daftarRuangan;
     }
 
-    public void addRuangan(Room newruang){
-        daftarRuangan.add(newruang);
+    public void addRuangan(String newRoom, Room referenceRoom, String arah) {
+        Room ruanganBaru = new Room(newRoom, this);
+        //daftarRuangan.add(ruanganBaru);
+
+        referenceRoom.addNewRoom(newRuang.getNamaRuangan(), arah);
+        
+    }
+
+    public static void main(String[] args) {
+        House rumah1 = new House("H1");
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -35,56 +35,50 @@ public class Room {
 
     public void setRoomSpace(Room ruanglain, String arah){
         if (checkSpace(arah)){
-            roomSpace.replace(arah, "-", ruanglain.getNamaRuangan());
+            roomSpace.replace(arah.toLowerCase(), "-", ruanglain.getNamaRuangan());
         }
     }
 
-    public String reverseArah(String arah){
-        if (arah.equals("kiri")){
-            return "kanan";
-        }
-        else if (arah.equals("kanan")){
-            return "kiri";
-        }
-        else if (arah.equals("atas")){
-            return "bawah";
-        }
-        else if (arah.equals("bawah")){
-            return "atas";
-        }
-    }
+    // public String reverseArah(String arah){
+    //     if (arah.equals("kiri")){
+    //         return "kanan";
+    //     }
+    //     else if (arah.equals("kanan")){
+    //         return "kiri";
+    //     }
+    //     else if (arah.equals("atas")){
+    //         return "bawah";
+    //     }
+    //     else if (arah.equals("bawah")){
+    //         return "atas";
+    //     }
+    // }
 
     public boolean checkSpace(String arah) {
         boolean avail = false;
-        if (roomSpace.get(arah) == "-") {
+        if (roomSpace.get(arah.toLowerCase()) == "-") {
             avail = true;
         }
         return avail;
     }
 
-    public void addRoom(Room ruangan, String arah) {
-        if (!ruangan.getHouse().equals(rumah)) { // Nanti rencananya mau dibuat exception
-            System.out.println("Gagal menambahkan ruangan: Ruangan tidak berada di dalam rumah yang sama!");
-        } else {
-            //boolean available = checkSpace(arah);
-            if (checkSpace(arah) && ruangan.checkSpace(reverseArah(arah))) {
-                setRoomSpace(ruangan, arah);
-                //Bingung cara insert ruang ini ke roomspacenya ruangan.
-                //Idenya setRoomSpace(ruangini, reverseArah(arah))
-            }
+    public void addNewRoom(String namaRuangan, String arah) {
+        Room ruanganBaru = new Room(namaRuangan, this.rumah);
+        if (checkSpace(arah)) {
+            setRoomSpace(ruanganBaru, arah);
+            //Bingung cara insert ruang ini ke roomspacenya ruangan.
+            //Idenya setRoomSpace(ruangini, reverseArah(arah))
         }
     }
     
-    // public static void main(String[] args) {
-    //     House rumah1 = new House("H1");
-    //     Room ruang1 = new Room("R1", rumah1);
-    //     ruang1.printPetaRuangan();
-    //     System.out.println(ruang1.roomSpace.toString());
-    //     Room ruang2 = new Room("R2", rumah1);
+    public static void main(String[] args) {
+        House rumah1 = new House("H1");
+        Room ruang1 = new Room("R1", rumah1);
+        ruang1.printPetaRuangan();
+        System.out.println(ruang1.roomSpace.toString());
 
-    //     boolean available = ruang1.checkSpace("kiri");
-    //     System.out.println(available);
-    //     ruang1.addRoom(ruang2, "kiri");
-    //     System.out.println(ruang1.roomSpace.toString());
-    // }
+        System.out.println(ruang1.checkSpace("KIRI"));
+        ruang1.addNewRoom("R2", "kiRi");
+        System.out.println(ruang1.roomSpace.toString());
+    }
 }
