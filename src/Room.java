2caf662396f18e3e9423c99ee1be9ddc4ae5df7b
arifu11 <arifuddinachmad11@gmail.com>
2023-04-26 10:28:30 +0700

    Delete atribut roomspace

--- src/Room.java
+++ src/Room.java
@@ -5,7 +5,7 @@ public class Room {
     private String namaRuangan;
     private House rumah;
     private Matrix petaRuangan;
-    private HashMap<String, String> roomSpace;
+    //private HashMap<String, String> roomSpace;
     private HashMap<String, Point> placedObject;
 
     public Room(String namaRuangan, House rumah) {
@@ -14,11 +14,11 @@ public class Room {
         petaRuangan = new Matrix(6, 6);
         placedObject = new HashMap<String, Point>();
 
-        roomSpace = new HashMap<String, String>();
-        roomSpace.put("kiri", "-");
-        roomSpace.put("kanan", "-");
-        roomSpace.put("atas", "-");
-        roomSpace.put("bawah", "-");
+        // roomSpace = new HashMap<String, String>();
+        // roomSpace.put("kiri", "-");
+        // roomSpace.put("kanan", "-");
+        // roomSpace.put("atas", "-");
+        // roomSpace.put("bawah", "-");
     }
 
     public String getNamaRuangan() {
@@ -33,47 +33,47 @@ public class Room {
         petaRuangan.printMatrix();
     }
 
-    public void setRoomSpace(Room ruanglain, String arah){
-        if (checkSpace(arah)){
-            roomSpace.replace(arah, "-", ruanglain.getNamaRuangan());
-        }
-    }
+    // public void setRoomSpace(Room ruanglain, String arah){
+    //     if (checkSpace(arah)){
+    //         roomSpace.replace(arah, "-", ruanglain.getNamaRuangan());
+    //     }
+    // }
 
-    public String reverseArah(String arah){
-        if (arah.equals("kiri")){
-            return "kanan";
-        }
-        else if (arah.equals("kanan")){
-            return "kiri";
-        }
-        else if (arah.equals("atas")){
-            return "bawah";
-        }
-        else if (arah.equals("bawah")){
-            return "atas";
-        }
-    }
+    // public String reverseArah(String arah){
+    //     if (arah.equals("kiri")){
+    //         return "kanan";
+    //     }
+    //     else if (arah.equals("kanan")){
+    //         return "kiri";
+    //     }
+    //     else if (arah.equals("atas")){
+    //         return "bawah";
+    //     }
+    //     else if (arah.equals("bawah")){
+    //         return "atas";
+    //     }
+    // }
 
-    public boolean checkSpace(String arah) {
-        boolean avail = false;
-        if (roomSpace.get(arah) == "-") {
-            avail = true;
-        }
-        return avail;
-    }
+    // public boolean checkSpace(String arah) {
+    //     boolean avail = false;
+    //     if (roomSpace.get(arah) == "-") {
+    //         avail = true;
+    //     }
+    //     return avail;
+    // }
 
-    public void addRoom(Room ruangan, String arah) {
-        if (!ruangan.getHouse().equals(rumah)) { // Nanti rencananya mau dibuat exception
-            System.out.println("Gagal menambahkan ruangan: Ruangan tidak berada di dalam rumah yang sama!");
-        } else {
-            //boolean available = checkSpace(arah);
-            if (checkSpace(arah) && ruangan.checkSpace(reverseArah(arah))) {
-                setRoomSpace(ruangan, arah);
-                //Bingung cara insert ruang ini ke roomspacenya ruangan.
-                //Idenya setRoomSpace(ruangini, reverseArah(arah))
-            }
-        }
-    }
+    // public void addRoom(Room ruangan, String arah) {
+    //     if (!ruangan.getHouse().equals(rumah)) { // Nanti rencananya mau dibuat exception
+    //         System.out.println("Gagal menambahkan ruangan: Ruangan tidak berada di dalam rumah yang sama!");
+    //     } else {
+    //         //boolean available = checkSpace(arah);
+    //         if (checkSpace(arah) && ruangan.checkSpace(reverseArah(arah))) {
+    //             setRoomSpace(ruangan, arah);
+    //             //Bingung cara insert ruang ini ke roomspacenya ruangan.
+    //             //Idenya setRoomSpace(ruangini, reverseArah(arah))
+    //         }
+    //     }
+    // }
     
     // public static void main(String[] args) {
     //     House rumah1 = new House("H1");
