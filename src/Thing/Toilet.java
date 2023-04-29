727444febdc1dcf8aa59dc01b0e880011ed7d97c
CarissaZ <92920009+CarissaZ@users.noreply.github.com>
2023-04-29 21:26:04 +0700

    Update Toilet.java

--- src/Thing/Toilet.java
+++ src/Thing/Toilet.java
@@ -2,12 +2,13 @@ package src.Thing;
 import src.Sim;
 
 public class Toilet extends Thing {
-    public Toilet(){
-        super("Toilet", 1, 1, 50);
+    public Toilet(String kodeItem){
+        super("Toilet", kodeItem, 1, 1, 50);
     }
     
     public void buangAir(Sim sim){
         sim.changeMood(10);
+        // masih bingung
         sim.changeKekenyangan(-20);
     }
 
