3f7aae4a1f8f11b09db44abaf942c0e8526b9060
samuel-eric <samueltan086@gmail.com>
2023-04-29 22:51:15 +0700

    memperbaiki upgrade rumah di main dan kompor

--- src/Thing/Kompor.java
+++ src/Thing/Kompor.java
@@ -1,15 +1,15 @@
 package src.Thing;
+
 import src.*;
 
 public abstract class Kompor extends Thing implements Cook {
-    public Kompor(String nama, String kodeItem, int panjang, int lebar, int harga){
+    public Kompor(String nama, String kodeItem, int panjang, int lebar, int harga) {
         super(nama, kodeItem, panjang, lebar, harga);
     }
-    
-    public void Cooking(Sim sim, Food food){
-        int waktu;
+
+    public void Cooking(Sim sim, Food food) {
+        double waktu;
         waktu = 1.5 * food.getKekenyangan();
-        sim.setMood((sim.getMood()) + 10);
+        sim.changeMood(10);
     }
 }
-
