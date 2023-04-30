5fa6b2ebb0d5e87f1aa411525b5aec7689371344
vaniaasabila <88761105+vaniaasabila@users.noreply.github.com>
2023-05-01 00:11:19 +0700

    Update GameManajer.java

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,22 +1,19 @@
 package src;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class GameManager {
     private World world;
     private ArrayList<Sim> simList;
-    private int time;
-    private int hari;
-    private ArrayList<String> listOfActions;
+    private Timer worldTimer;
     private Sim activeSim;
     private int houseCount;
+    private int kasurCount = 0;
 
     public GameManager(){
         world = World.getWorld();
         simList = new ArrayList<Sim>();
-        hari = 0;
-        listOfActions = new ArrayList<String>();
+        worldTimer = world.getTimer();
         houseCount = 0;
     }
 
@@ -28,24 +25,36 @@ public class GameManager {
         return simList;
     }
 
-    public int getHari(){
-        return hari;
+    public Timer getWorldTimer(){
+        return worldTimer;
     }
 
-    public ArrayList<String> getListOfActions(){
-        return listOfActions;
+    public Sim getSim(String namaSim){
+        Iterator<Sim> simIterator = simList.iterator();
+        boolean found = false;
+        Sim targetSim = null;
+        while (!found && simIterator.hasNext()){
+            targetSim = simIterator.next();
+            if (targetSim.getNamaLengkap().equals(namaSim)){
+                found = true;
+            }
+            else{
+                targetSim = null;
+            }
+        }
+        return targetSim;
     }
 
     public Sim getActiveSim(){
         return activeSim;
     }
 
-    public int getTime(){
-        return time;
-    }
-
     public int getHouseCount(){
         return houseCount;
+    }
+
+    public int getKasurCount(){
+        return kasurCount;
     }
 
     public void printSimList(){
@@ -56,44 +65,65 @@ public class GameManager {
         }
     }
 
-    // Setter
+    // Setter dan Adder
     public void setActiveSim(Sim sim){
         activeSim = sim;
     }
 
-    public void setHari(int hari){
-        this.hari = hari;
+    public void addSim(String sim){
+        Sim newSim = new Sim(sim);
+        this.simList.add(newSim);
+        setActiveSim(newSim);
+    }
+
+    public void addNewHouse(Sim sim){
+        String kodeRumahBaru = "H" + houseCount;
+        while(Objects.isNull(world.getHouse(kodeRumahBaru))){
+            try{
+                world.addHouse(new Random().nextInt(64) + 1, new Random().nextInt(64) + 1, kodeRumahBaru);
+            }
+            catch(Exception e){
+            }
+        }
+    }
+
+    public void addNewHouse(Sim sim, int x, int y){
+        String kodeRumahBaru = "H" + houseCount;
+        try{
+            world.addHouse(x, y, kodeRumahBaru);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     // Menu
     // TODO : Implementasi Help
     public void help(){
-        System.out.println("\033[1;32mThis is the help information for the Sim-Plicity.");
 
-        System.out.println("\033[1;91m====================================================================================================================================================================");
+        System.out.println("\033[1;92m█░█ █▀▀ █░░ █▀█");
+        System.out.println("\033[1;92m█▀█ ██▄ █▄▄ █▀▀\n\n");
+        System.out.println("\033[1;92mThis is the help information for the Sim-Plicity.\n\n");
+
+        System.out.println("\033[1;34m1. GAME DESCRIPTION\n");
+        System.out.println("\033[1;34m2. HOW TO PLAY?\n");
+        System.out.println("\033[1;34m3. COMMAND LIST\n");
+
         
-        System.out.println("\033[1;91m ______   _______  _______  ___   _  ______   ___   _______  _______  ___     _______  _______  ______   __   __  _______  ___   __    _  _______  __    _ ");
-        System.out.println("\033[1;91m|      | |       ||       ||   | | ||    _ | |   | |       ||       ||   |   |       ||       ||    _ | |  |_|  ||   _   ||   | |  |  | ||   _   ||  |  | |");
-        System.out.println("\033[1;91m|  _    ||    ___||  _____||   |_| ||   | || |   | |    _  ||  _____||   |   |    _  ||    ___||   | || |       ||  |_|  ||   | |   |_| ||  |_|  ||   |_| |");
-        System.out.println("\033[1;91m| | |   ||   |___ | |_____ |      _||   |_|| |   | |   |_| || |_____ |   |   |   |_| ||   |___ |   |_|| |       ||       ||   | |       ||       ||       |");
-        System.out.println("\033[1;91m| |_|   ||    ___||_____  ||     |_ |    __ ||   | |    ___||_____  ||   |   |    ___||    ___||    __ ||       ||       ||   | |  _    ||       ||  _    |");
-        System.out.println("\033[1;91m|       ||   |___  _____| ||    _  ||   |  |||   | |   |     _____| ||   |   |   |    |   |___ |   |  ||| ||_|| ||   _   ||   | | | |   ||   _   || | |   |");
-        System.out.println("\033[1;91m|______| |_______||_______||___| |_||___|  |||___| |___|    |_______||___|   |___|    |_______||___|  |||_|   |_||__| |__||___| |_|  |__||__| |__||_|  |__|\n");
+        System.out.println("\033[1;91m===================================================================================");
+        
+        System.out.println("\033[1;91m█▀▀ ▄▀█ █▀▄▀█ █▀▀   █▀▄ █▀▀ █▀ █▀▀ █▀█ █ █▀█ ▀█▀ █ █▀█ █▄░█");
+        System.out.println("\033[1;91m█▄█ █▀█ █░▀░█ ██▄   █▄▀ ██▄ ▄█ █▄▄ █▀▄ █ █▀▀ ░█░ █ █▄█ █░▀█\n\n");
     
 
         System.out.println("\033[1;31mSim-plicity adalah permainan yang ingin dimiliki oleh Indra dan Doni. Game ini adalah permainan tentang kehidupan sehari-hari sims, karakter orang dalam game ini. \nSelain sim, game ini memiliki entitas world sebagai dunia virtual, rumah sebagai tempat tinggal sim, ruangan, dan objek-objek. \nSim juga memiliki kesejahteraan yang berparameter mood, kekenyangan, dan kesehatan yang bisa mempengaruhi umur sim. \nSim juga memiliki pekerjaan dan dapat melakukan aksi yang bisa mempengaruhi parameter kesejahteraannya.");
         System.out.println("\033[1;31mGame ini diprogram menggunakan bahasa Java dengan berbasis Command Line Interface. \nPertama, pemain dapat menciptakan sim. Setiap pembuatan satu sim, user akan mendapatkan satu rumah baru berukuran 6x6.  \nUser dapat berpindah-pindah sim untuk memainkannya. Untuk cara bermain, akan dijelaskan lebih rinci pada bagian cara bermain. \n");
-        System.out.println("\033[1;91m====================================================================================================================================================================\n\n\n\n");
+        System.out.println("\033[1;91m===================================================================================\n\n\n\n");
     
 
-        System.out.println("\033[1;93m====================================================================================================================================================================");
-        System.out.println("\033[1;93m _______  _______  ______   _______    _______  _______  ______   __   __  _______  ___   __    _ ");
-        System.out.println("\033[1;93m|       ||   _   ||    _ | |   _   |  |  _    ||       ||    _ | |  |_|  ||   _   ||   | |  |  | |");
-        System.out.println("\033[1;93m|       ||  |_|  ||   | || |  |_|  |  | |_|   ||    ___||   | || |       ||  |_|  ||   | |   |_| |");
-        System.out.println("\033[1;93m|       ||       ||   |_|| |       |  |       ||   |___ |   |_|| |       ||       ||   | |       |");
-        System.out.println("\033[1;93m|      _||       ||    __ ||       |  |  _   | |    ___||    __ ||       ||       ||   | |  _    |");
-        System.out.println("\033[1;93m|     |_ |   _   ||   |  |||   _   |  | |_|   ||   |___ |   |  ||| ||_|| ||   _   ||   | | | |   |");
-        System.out.println("\033[1;93m|_______||__| |__||___|  |||__| |__|  |_______||_______||___|  |||_|   |_||__| |__||___| |_|  |__|" + "\n");
+        System.out.println("\033[1;93m========================================================"); 
+        System.out.println("\033[1;93m█░█ █▀█ █░█░█   ▀█▀ █▀█   █▀█ █░░ ▄▀█ █▄█ ▀█");
+        System.out.println("\033[1;93m█▀█ █▄█ ▀▄▀▄▀   ░█░ █▄█   █▀▀ █▄▄ █▀█ ░█░ ░▄\n");
 
         System.out.println("\033[1;33m1. Buatlah Sim baru! Nantinya, kamu juga bisa memilih sim yang ingin kamu mainkan.");
         System.out.println("\033[1;33m2. Setiap kamu membuat Sim baru, kondisi kesejahteraan kamu (kekenyangan, mood, dan kesehatan) akan otomatis di-set 80 dan kamu juga akan diberi uang awal sebesar 100.");
@@ -148,10 +178,35 @@ public class GameManager {
         System.out.println("      \033[1;30ml)Lukisan ");
         System.out.println("      \033[1;30mm)Toilet ");
         System.out.println("      \033[1;30mn)Wastafel ");
-        System.out.println("      \033[1;30mo)Cermin ");
+        System.out.println("      \033[1;30mo)Cermin \n");
 
-        System.out.println("\033[1;93m====================================================================================================================================================================");
+        System.out.println("\033[1;93m========================================================\n\n\n\n");    
+    
+        System.out.println("\033[1;96m==============================================================");    
+        System.out.println("\033[1;96m█▀▀ █▀█ █▀▄▀█ █▀▄▀█ ▄▀█ █▄░█ █▀▄   █░░ █ █▀ ▀█▀"); 
+        System.out.println("\033[1;96m█▄▄ █▄█ █░▀░█ █░▀░█ █▀█ █░▀█ █▄▀   █▄▄ █ ▄█ ░█░\n"); 
+
+        System.out.println("\033[1;36mBerikut adalah daftar command yang dapat digunakan : \n");
+        System.out.println("\033[1;36m1. START GAME");
+        System.out.println("\033[1;36m2. HELP");
+        System.out.println("\033[1;36m3. EXIT");
+        System.out.println("\033[1;36m4. VIEW SIM INFO");
+        System.out.println("\033[1;36m5. VIEW CURRENT LOCATION");
+        System.out.println("\033[1;36m6. VIEW INVENTORY");
+        System.out.println("\033[1;36m7. UPGRADE RUMAH");
+        System.out.println("\033[1;36m8. MOVE ROOM");
+        System.out.println("\033[1;36m9. EDIT ROOM");
+        System.out.println("\033[1;36m10. ADD SIM");
+        System.out.println("\033[1;36m11. CHANGE SIM");
+        System.out.println("\033[1;36m12. LIST OBJECT");
+        System.out.println("\033[1;36m13. GO TO OBJECT");
+        System.out.println("\033[1;36m14. ACTION\n");
+
+        System.out.println("\033[1;96m==============================================================");    
+
     }
+
+
 
     // TODO : Implementasi Exit
     public void exit(){
@@ -172,16 +227,36 @@ public class GameManager {
 
     // TODO : Implementasi viewSimInfo
     public void viewSimInfo(){
-
+        getActiveSim().showSimInfo();
     }
 
     // TODO : Implementasi viewCurrentLocation
     public void viewCurrentLocation(){
-
+        System.out.println("Lokasi saat ini : " + getActiveSim().getCurrentPos().toString());
+        //Ini koordinat aja atau sampe ke rumah dan ruangan juga?
     }
 
     public void viewInventory(){
         activeSim.getInventory().printItems();
+    }
+
+    public void runTime(){
+        Sim firstActiveSim = null;
+        Iterator<Sim> simIterator = simList.iterator();
+        boolean stop = false;
+        while (!stop && simIterator.hasNext()){
+            firstActiveSim = simIterator.next();
+            if (firstActiveSim.getStatus().equals("active")){
+                stop = true;
+            }
+            else if (!simIterator.hasNext()){
+                firstActiveSim = null;
+            }
+        }
+        
+        if (!Objects.isNull(firstActiveSim)){
+            worldTimer.reduceTime(1, this);
+        }
     }
 
 }
