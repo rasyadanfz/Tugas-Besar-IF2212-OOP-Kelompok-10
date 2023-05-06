650bbc9391f2881c89bfcc146d6c64f249e7e02a
rasyadanfz <18221103@itb.ac.id>
2023-05-06 17:20:53 +0700

    fix cooking

--- src/main/java/com/Kelompok10/Thing/Kompor.java
+++ src/main/java/com/Kelompok10/Thing/Kompor.java
@@ -12,17 +12,25 @@ public class Kompor extends ActiveItems implements Cook {
     }
 
     public void Cooking(Sim sim, String namaMakanan) {
-        Food food = new Food(namaMakanan);
-        int duration = (int) (1.5 * food.getKekenyangan());
-        Action actionCook = new Action("cooking", duration, this);
-        sim.addAction(actionCook);
-        sim.setStatus("active");
-        sim.getInventory().addItem(food);
-        effect(sim, actionCook);
-        sim.incNotSleepYet(duration);
-        if (sim.getHaveEat())
-            sim.incNotPeeYet(duration);
-        sim.getNegativeEffect();
+        try {
+            Food food = new Food(namaMakanan);
+            int duration = (int) (1.5 * food.getKekenyangan());
+            Action actionCook = new Action("cooking", duration, this);
+            sim.addAction(actionCook);
+            sim.setStatus("active");
+            sim.setInActiveAction(true);
+            effect(sim, actionCook);
+            sim.getInventory().addItem(food);
+            for (Ingredient ing : food.getBahanMakanan()) {
+                sim.getInventory().removeItem(ing.getNama());
+            }
+            sim.incNotSleepYet(duration);
+            if (sim.getHaveEat())
+                sim.incNotPeeYet(duration);
+            sim.getNegativeEffect();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void effect(Sim sim, Action action) {
@@ -53,24 +61,24 @@ public class Kompor extends ActiveItems implements Cook {
     public boolean checkBahanMasak(Inventory<Item> inventory) {
         boolean bisaMasak = false;
         System.out.println("Berikut adalah daftar makanan yang bisa dimasak: ");
-        if (inventory.containsItem("nasi") && inventory.containsItem("ayam")) {
+        if (inventory.containsItem("Nasi") && inventory.containsItem("Ayam")) {
             System.out.println("- Nasi Ayam");
             bisaMasak = true;
         }
-        if (inventory.containsItem("nasi") && inventory.containsItem("kentang") && inventory.containsItem("wortel")
+        if (inventory.containsItem("Nasi") && inventory.containsItem("Kentang") && inventory.containsItem("Wortel")
                 && inventory.containsItem("sapi")) {
             System.out.println("- Nasi Kari");
             bisaMasak = true;
         }
-        if (inventory.containsItem("susu") && inventory.containsItem("kacang")) {
+        if (inventory.containsItem("Susu") && inventory.containsItem("Kacang")) {
             System.out.println("- Susu Kacang");
             bisaMasak = true;
         }
-        if (inventory.containsItem("wortel") && inventory.containsItem("bayam")) {
+        if (inventory.containsItem("Wortel") && inventory.containsItem("Bayam")) {
             System.out.println("- Tumis Sayur");
             bisaMasak = true;
         }
-        if (inventory.containsItem("kentang") && inventory.containsItem("sapi")) {
+        if (inventory.containsItem("Kentang") && inventory.containsItem("Sapi")) {
             System.out.println("- Bistik");
             bisaMasak = true;
         }
